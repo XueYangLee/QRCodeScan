28633e342d7dbbe124fb668b60031d613926d4e3
Singularity_Lee <496736912@qq.com>
2018-09-03 11:31:18 +0800

    Initial Commit

---
+++ QRCodeScan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QRCodeScan
+//
+//  Created by 李雪阳 on 2018/9/3.
+//  Copyright © 2018年 singularity. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
